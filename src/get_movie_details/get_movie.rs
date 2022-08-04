1addeb165d27f8ffbf8a6a9b060827cf3b56ce7c
furkancakar <cakar.55.furkan@icloud.com>
2022-08-05 00:47:53 +0300

    crate import error

--- src/get_movie_details/get_movie.rs
+++ src/get_movie_details/get_movie.rs
@@ -1,11 +1,12 @@
 use crate::get_movie_structs::MovieMovieId;
 
-pub(crate) async fn movie_movie_id(search_query: String) -> MovieMovieId {
-    let mut movie_url = String::from("https://api.themoviedb.org/3/search/movie?api_key=4e110fd06d91f1f01af5acd9fa42a82d&language=en-US&query=");
-    movie_url.push_str(&*search_query.replace(" ", "%20"));
-    movie_url.push_str("&page=1&include_adult=false");
+pub(crate) async fn get_movie_details(movie_tmdb_id: String) -> MovieMovieId {
+    let mut movie_url = String::from("https://api.themoviedb.org/3/movie/");
+    movie_url.push_str(&*movie_tmdb_id.replace(" ", "%20"));
+    movie_url.push_str("&?api_key=4e110fd06d91f1f01af5acd9fa42a82d&language=en-US");
 
-    let search_results: SearchMovie = reqwest::Client::new()
+    println!("!!!!!-----1!!!!!!{}", movie_url);
+    let get_movie_result: MovieMovieId = reqwest::Client::new()
         .get(movie_url)
         .send()
         .await
@@ -13,5 +14,5 @@ pub(crate) async fn movie_movie_id(search_query: String) -> MovieMovieId {
         .json()
         .await
         .unwrap();
-    search_results
+    get_movie_result
 }

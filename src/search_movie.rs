71e5084bfcb5091ebdab69fe68c4d50f2602c473
furkancakar <cakar.55.furkan@icloud.com>
2022-08-05 00:50:51 +0300

    github connection removed

--- src/search_movie.rs
+++ src/search_movie.rs
@@ -1,7 +1,5 @@
 use crate::structs::SearchMovie;
 
-
-
 pub(crate) async fn search_movie(search_query: String) -> SearchMovie {
     let mut movie_url = String::from("https://api.themoviedb.org/3/search/movie?api_key=4e110fd06d91f1f01af5acd9fa42a82d&language=en-US&query=");
     movie_url.push_str(&*search_query.replace(" ", "%20"));
